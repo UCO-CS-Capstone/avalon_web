2bb7fc777253c2abc566bc42c016575ff1144907
Gabe Cook <gabe565@users.noreply.github.com>
2018-03-06 20:10:22 -0600

    Initial CI config

--- .travis/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
-
-scp build/libs/ROOT.war git@$server:$destination_file
