09c97d65f7a90e89b9492298c2808aea7a6e5125
Gabe Cook <gabe565@users.noreply.github.com>
2018-03-08 21:48:15 -0600

    Update TravisCI config to update db on push

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,4 +4,4 @@
 rsync -q --inplace "$TRAVIS_BUILD_DIR/build/libs/ROOT.war" "git@$server:$destination_file"
 
 # Update database from schema
-cat "$TRAVIS_BUILD_DIR/src/sql/avalon_db.sql" | ssh "$server" "mysql -uroot -p$db_pass"
+cat "$TRAVIS_BUILD_DIR/src/sql/avalon_db.sql" | ssh "git@$server" "mysql -uroot -p$db_pass"
