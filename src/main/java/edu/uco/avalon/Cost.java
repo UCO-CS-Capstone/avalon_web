d1c3c6f2ce9ad522069aa4f95b2b536e270a84b8
tianyi chen <tchen8@uco.edu>
2018-04-04 16:03:06 -0500

    Tianyi final sprint

---
+++ src/main/java/edu/uco/avalon/Cost.java
@@ -0,0 +1,29 @@
+package edu.uco.avalon;
+
+public class Cost {
+
+    private String name;
+    private double cost;
+
+
+    public Cost(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+}
